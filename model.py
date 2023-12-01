1b4fe3bc7394a447d0559fc1f6892f24a981f5cd
christopher.baez <christopherbaez8@gmail.com>
2023-11-30 21:31:19 -0600

    end of day upload

--- model.py
+++ model.py
@@ -74,47 +74,5 @@ def preprocess_telco(train, validate, test):
 
     return encoded_train, encoded_validate, encoded_test
 
-from sklearn.metrics import classification_report, confusion_matrix
 
-def print_classification_metrics(y_true, y_pred):
-    """
-    Prints classification metrics for a given set of true and predicted values.
-
-    Parameters:
-    y_true (array-like): True labels of the data.
-    y_pred (array-like): Predicted labels from the model.
-
-    Returns:
-    None. Prints the classification metrics.
-    """
-    # Calculating confusion matrix
-    tn, fp, fn, tp = confusion_matrix(y_true, y_pred).ravel()
-
-    # Calculating metrics
-    accuracy = (tp + tn) / (tp + tn + fp + fn)
-    true_positive_rate = tp / (tp + fn)
-    false_positive_rate = fp / (fp + tn)
-    true_negative_rate = tn / (tn + fp)
-    false_negative_rate = fn / (fn + tp)
-    precision = tp / (tp + fp)
-    recall = tp / (tp + fn)  # recall is the same as true_positive_rate
-    f1_score = 2 * (precision * recall) / (precision + recall)
-
-    # Printing the metrics
-    print("Classification Metrics:")
-    print(f"Accuracy: {accuracy:.2f}")
-    print(f"True Positive Rate: {true_positive_rate:.2f}")
-    print(f"False Positive Rate: {false_positive_rate:.2f}")
-    print(f"True Negative Rate: {true_negative_rate:.2f}")
-    print(f"False Negative Rate: {false_negative_rate:.2f}")
-    print(f"Precision: {precision:.2f}")
-    print(f"Recall: {recall:.2f}")
-    print(f"F1 Score: {f1_score:.2f}")
-
-    # Printing classification report for additional metrics and support
-    print("\nClassification Report:")
-    print(classification_report(y_true, y_pred))
-
-# Example usage
-# print_classification_metrics(y_test, y_pred)
 
